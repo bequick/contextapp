d86e2f06bba86b01adf85df1e040a86344890ec1
Marcos <mdonoso2007@gmail.com>
2022-01-23 16:04:35 -0300

    se agrega control de text

--- src/components/Provider.js
+++ src/components/Provider.js
@@ -1,9 +1,16 @@
+import React, {useState} from "react";
 import Context from "../context/Context";
 
-class Provider extends Component{
-    render(){
-        return (<Context.Provider value={{symbol:'BTC'}}><b>hola</b></Context.Provider>);
-    }
-}
+const Provider = () => {
 
+    const[value, setValue] = useState("")
+
+    const changeValue=(event)=>{
+        console.log(event.target.value)
+        setValue(event.target.value)
+    }
+
+    return <input type="text" name="txtSymbol" id="txtSymbol" placeholder="BTC" onChange={changeValue} />
+
+}
 export default Provider
